2801895aaa40ada13fe26066ccd2f2b37fc896f9
holyshitt <izvekovp429@gmail.com>
2023-01-31 20:23:07 +0300

    правки в валидации

--- classes/SheetDataValidator.py
+++ classes/SheetDataValidator.py
@@ -83,7 +83,7 @@ class Validator(Singleton):
             elif fieldtype == FieldType.string:
                 if not required and not elem:
                     continue
-                if len(elem) > 4095:
+                if len(elem) > 4094:
                     self.failure('Поле %s не должно быть больше 4096 символов' % fields['field_name'])
                 if '/' in elem:
                     self.failure('Поле \"%s\" не должно содержать символ "/"' % fields['field_name'])
